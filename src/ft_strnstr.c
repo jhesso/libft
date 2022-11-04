405d43d8d84b5fc712a308ada4709657c9013fb0
Jhesso <juho.hesso@hotmail.com>
2022-11-04 15:53:42 +0200

    fixed strnstr. had a problem with not resetting the found variable. now using a totally new logic

--- src/ft_strnstr.c
+++ src/ft_strnstr.c
@@ -6,37 +6,90 @@
 /*   By: jhesso <jhesso@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 21:43:16 by jhesso            #+#    #+#             */
-/*   Updated: 2022/10/28 22:05:29 by jhesso           ###   ########.fr       */
+/*   Updated: 2022/11/04 15:46:32 by jhesso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+// char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+// {
+// 	char	*hay;
+// 	char	*need;
+// 	int		found;
+// 	int		needle_size;
+
+// 	hay = (char *)haystack;
+// 	need = (char *)needle;
+// 	found = 0;
+// 	needle_size = ft_strlen(need);
+// 	if (need[0] == '\0')
+// 		return (hay);
+// 	while (len != 0 && *hay != '\0')
+// 	{
+// 		if ((found > 0 && *hay - 1 == *need - 1 && *hay == *need)
+// 			|| (found == 0 && *hay == *need))
+// 		{
+// 			found++;
+// 			need++;
+// 		}
+// 		if (found == needle_size)
+// 			return (hay - found + 1);
+// 		hay++;
+// 		len--;
+// 	}
+// 	return (NULL);
+// }
+
+// char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+// {
+// 	char	*hay;
+// 	char	*need;
+// 	int		found;
+// 	int		needle_size;
+
+// 	hay = (char *)haystack;
+// 	need = (char *)needle;
+// 	found = 0;
+// 	needle_size = ft_strlen(need);
+// 	if (need[0] == '\0')
+// 		return (hay);
+// 	while (len != 0 && *hay != '\0')
+// 	{
+// 		while ((found > 0 && *hay - 1 == *need - 1 && *hay == *need)
+// 			|| (found == 0 && *hay == *need))
+// 		{
+// 			found++;
+// 			need++;
+// 			hay++;
+// 		}
+// 		if (found == needle_size)
+// 			return (hay - found + 1);
+// 		need = need - found;
+// 		found = 0;
+// 		len--;
+// 	}
+// 	return (NULL);
+// }
+
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	char	*hay;
-	char	*need;
-	int		found;
-	int		needle_size;
+	size_t	i;
+	size_t	j;
 
-	hay = (char *)haystack;
-	need = (char *)needle;
-	found = 0;
-	needle_size = ft_strlen(need);
-	if (need[0] == '\0')
-		return (hay);
-	while (len != 0 && *hay != '\0')
+	i = 0;
+	if (needle == NULL || *needle == '\0')
+		return ((char *)haystack);
+	while (haystack[i] != '\0' && i < len)
 	{
-		if ((found > 0 && *hay - 1 == *need - 1 && *hay == *need)
-			|| (found == 0 && *hay == *need))
+		j = 0;
+		while (needle[j] == haystack[i + j] && i + j < len)
 		{
-			found++;
-			need++;
+			if (needle[j + 1] == '\0')
+				return ((char *)haystack + i);
+			j++;
 		}
-		if (found == needle_size)
-			return (hay - found + 1);
-		hay++;
-		len--;
+		i++;
 	}
 	return (NULL);
 }
