76f8acfad698b50fc7967154822d4d04515f252e
Jhesso <juho.hesso@hotmail.com>
2022-11-10 18:10:45 +0200

    thu 10.11.22

---
+++ ft_split_notes.md
@@ -0,0 +1,34 @@
+# ft_split()
+
+**Function name:** ft_split<br>
+**Prototype char:** **ft_split(char const *s, char c);<br>
+**Turn in files:** - <br>
+### **Parameters:**
+- s: The string to be split. <br>
+- c: The delimiter character.<br>
+
+### **Return value:**
+- The array of new strings resulting from the split.<br>
+- NULL if the allocation fails.
+
+**External functions:** malloc, free<br>
+**Description:** Allocates (with malloc(3)) and returns an array
+of strings obtained by splitting ’s’ using the
+character ’c’ as a delimiter. The array must end
+with a NULL pointer.
+
+-----------
+
+## Solution:
+1. calculate how many different strings we will need (its basically the amount of times that c occurs in s)
+2. allocate memory for the strings needed
+	- only allocate enough for the strings and not too much
+3. create new strings from s
+
+## Known bugs:
+- the delimiter character is saved to the string
+- the last part is not saved (maybe over written to NULL)
+- if more than one delimiter is between the words, that delimiter is included in the string
+- the word counting has an obvious oversight. It counts the amount of delimiter characters. -> if more than one delimiter is betweem words, we count all of them as words meaning the amount calculater is too many compared to what we actually need.
+
+getting some segfaults atm. seems to count delimiters after the string ended.
--- src/ft_split.c
+++ src/ft_split.c
@@ -6,13 +6,111 @@
 /*   By: jhesso <jhesso@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 14:59:21 by jhesso            #+#    #+#             */
-/*   Updated: 2022/11/10 15:00:14 by jhesso           ###   ########.fr       */
+/*   Updated: 2022/11/10 17:43:17 by jhesso           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "test/test.h"
+
+static int	get_amount_of_strings(char const *s, char c)
+{
+	int	amount;
+	int	i;
+
+	amount = 0;
+	i = 0;
+	while (s[i] == c && s[i] != '\0')
+		i++;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			amount++;
+			while (s[i] == c)
+				i++;
+		}
+		i++;
+	}
+	if (s[i] == '\0' && s[i - 1] == c)
+		amount--;
+	return (amount);
+}
+
+static	int	get_str_len(char const *s, char c, int start)
+{
+	int	i;
+
+	i = start;
+	while (s[i] != '\0' && s[i] != c)
+		i++;
+	return (i);
+}
+
+static char	**allocate_mem(char const *s, char c, int str_amount)
+{
+	char	**ret;
+	int		len;
+	int		i;
+	int		start;
+
+	ret = malloc(sizeof(char **) * str_amount + 1); // allocate memory for the 2d array
+	if (ret == NULL)
+		return (NULL);
+	i = 0;
+	start = 0;
+	while (i < str_amount)
+	{
+		len = get_str_len(s, c, start);
+		start = start + len; // calculate the starting point of the next string
+		ret[i] = malloc(sizeof(char *) * len + 1); // allocate memory for each string
+		if (ret[i] == NULL)
+			return (NULL);
+		i++;
+	}
+	return (ret);
+}
+
+static char	**create_string(char const *s, char c, char **ret, int str_amount)
+{
+	int	row;
+	int	col;
+	int	i;
+
+	row = 0;
+	i = 0;
+	col = 0;
+	while (s[i] != '\0' && row < str_amount)
+	{
+		if (s[i] == c)
+		{
+			while (s[i] == c && s[i + 1] != '\0')
+				i++;
+			row++;
+			col = 0;
+		}
+		ret[row][col] = s[i];
+		col++;
+		i++;
+	}
+	row++;
+	ret[row] = NULL;
+	return (ret);
+}
 
 char	**ft_split(char const *s, char c)
 {
-	return (NULL);
+	int		str_amount;
+	char	**ret;
+
+	if (s == NULL)
+		return (NULL);
+	printf("stepping into get_amount_of_strings\n");
+	str_amount = get_amount_of_strings(s, c);
+	printf("amount of strings calculated: %d\n", str_amount);
+	printf("stepping into allocate_mem\n");
+	ret = allocate_mem(s, c, str_amount);
+	printf("stepping into create_string\n");
+	ret = create_string(s, c, ret, str_amount);
+	return (ret);
 }
